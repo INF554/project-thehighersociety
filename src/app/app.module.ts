b14e791848fa0840ed039cfed032917b8f18fd89
Tanay Shankar <tanaysh7@gmail.com>
2018-11-16 16:13:28 -0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Chart1Component } from './chart1/chart1.component';
+import { Chart2Component } from './chart2/chart2.component';
+import { Chart3Component } from './chart3/chart3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Chart1Component
+    Chart1Component,
+    Chart2Component,
+    Chart3Component
   ],
   imports: [
     BrowserModule
