5864ee66232958ba1a5afa8d83ad9db66f120cce
GUZiGit <zigu@usc.edu>
2018-11-27 22:07:30 -0800

    build function of year button

--- src/app/chart3/chart3.component.ts
+++ src/app/chart3/chart3.component.ts
@@ -33,28 +33,190 @@ export class Chart3Component implements OnInit {
 	var path=d3.geoPath()
 				.projection(projection)
 
+	// function drawMap() {
+	// 	d3.json("assets/worldmap.json").then(function (worldMap:any) {
+	// 		// d3.csv("assets/GDPOnly.csv").then(function(gdpData:any){
+
+				
+	// 			// draw map 
+
+	// 			var countries:any=feature(worldMap, worldMap.objects["custom.geo"])
+	// 			countries = countries.features
+	// 			// console.log(countries)
+	// 			var paths= svg.selectAll(".country")
+	// 						.data(countries)
+	// 						.enter()
+	// 						.append("path")
+	// 						.attr("class","country")
+	// 						.attr("d",path)
+	// 						.attr("fill","None")
+	// 						.attr("stroke","black")
+	// 						.on("mouseover",function(){
+	// 							var selected:any = this
+								
+	// 							// console.log(selected.__data__.properties.name)
+
+	// 							// d3.select(selected)
+	// 								// .attr("fill","red")
+	// 						})
+	// 						.on("mouseout",function(){
+	// 							var selected:any = this
+
+	// 							// d3.select(selected)
+	// 								// .attr("fill","None")
+	// 						})
+	// 						.on("click",function(){
+	// 							// var selected:any = this
+	// 							// d3.select(selected)
+	// 								// .attr("fill","green")
+	// 						})
+
+	// 			// var selection = d3.select('select').property('value');
+	// 			// draw heat map with selected year
+	// 			// console.log(selection)
+	// 			// console.log(gdpData)
+
+	// 		// })
+
+			
+
+
+	// 	})
+	// }
+
+	// function drawGDP() {
+	// 	// body...
+	// 	d3.csv("assets/GDPOnly.csv").then(function(gdpData:any){
+
+	// 		function getTargetGDP(selectedYear:number) {
+	// 			var targetGDP:any = {}
+	// 			for (var i = 0; i < gdpData.length; ++i) {
+	// 				if(gdpData[i].Year==selectedYear) {
+	// 					// targetGDP.push({Country:gdpData[i].Country,value:+gdpData[i].Value})
+	// 					targetGDP[gdpData[i].Country] = +gdpData[i].Value
+	// 				}
+	// 			}
+	// 			return targetGDP
+	// 		}
+
+	// 		function getMaxGDP(targetGDP) {
+	// 			// @ts-ignore
+	// 			return Math.max(...Object.values(targetGDP))
+	// 		}
+
+	// 		var selectedYear = 2000;
+	// 		var targetGDP = getTargetGDP(selectedYear)
+	// 		var maxGDP = getMaxGDP(targetGDP)
+			
+	// 		console.log(maxGDP)
+
+	// 		// console.log(Object.keys(targetGDP))
+
+	// 		// var minG = Math.min(Object.values(targetGDP))
+
+	// 		// @ts-ignore
+	// 		var colorScale:any = d3.scaleLinear()
+	// 								.domain([0,maxGDP])
+	// 								// @ts-ignore
+	// 								.range([0,1])
+	// 								// .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')])
+ //      							// .interpolate(d3.)
+ //      							// .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')]);
+
+ //      		var paths= svg.selectAll(".country")
+	// 					.data(countries)
+	// 					.enter()
+	// 					.append("path")
+	// 					.attr("class","country")
+	// 					.attr("d",path)
+	// 					.attr("fill",function(d):any{
+	// 						var a:any = d
+	// 						// console.log(a.properties.name)
+	// 						// if(contryList.indexOf(a.properties.sovereignt)!=-1 && targetGDP[a.properties.sovereignt]) {
+	// 						// 	console.log(targetGDP[a.properties.sovereignt])
+	// 						// 	console.log(a.properties.sovereignt)
+	// 						// 	return d3.interpolateReds(colorScale(targetGDP[a.properties.sovereignt]))
+	// 						// }
+	// 						// return 'None'
+
+	// 						if(targetGDP[a.properties.sovereignt]) {
+	// 							// console.log(targetGDP[a.properties.sovereignt])
+	// 							// console.log(a.properties.sovereignt)
+	// 							return d3.interpolateReds(colorScale(targetGDP[a.properties.sovereignt]))
+	// 						}
+	// 						return 'None'
+
+	// 						// console.log(targetGDP[a.properties.name])
+	// 						// console.log(colorScale(targetGDP[a.properties.name]))
+	// 						// console.log(d3.schemeReds[colorScale(targetGDP[a.properties.name])])
+							
+	// 					})
+	// 					.attr("stroke","black")
+	// 					.on("mouseover",function(){
+	// 						var selected:any = this
+							
+	// 						// console.log(selected.__data__.properties.name)
+
+	// 						// d3.select(selected)
+	// 							// .attr("fill","red")
+	// 					})
+	// 					.on("mouseout",function(){
+	// 						var selected:any = this
+
+	// 						// d3.select(selected)
+	// 							// .attr("fill","None")
+	// 					})
+	// 					.on("click",function(){
+	// 						// var selected:any = this
+	// 						// d3.select(selected)
+	// 							// .attr("fill","green")
+	// 					})
+
+
+	// 	})
+	// }
+
+	// drawMap()
+
+	// drawGDP()
+
 
 	d3.json("assets/worldmap.json").then(function (worldMap:any) {
 		d3.csv("assets/GDPOnly.csv").then(function(gdpData:any){
 
-			function getTargetGDP(selectedYear:number) {
-				var targetGDP:any = {}
+			function getTargetGDP() {
+				var targetGDP = {}
+				// console.log(gdpData)
 				for (var i = 0; i < gdpData.length; ++i) {
-					if(gdpData[i].Year==selectedYear) {
-						// targetGDP.push({Country:gdpData[i].Country,value:+gdpData[i].Value})
-						targetGDP[gdpData[i].Country] = +gdpData[i].Value
+					if(Object.keys(targetGDP).indexOf(gdpData[i].Year)==-1) {
+						targetGDP[gdpData[i].Year] = {}
 					}
+					targetGDP[gdpData[i].Year][gdpData[i].Country] = +gdpData[i].Value
+
+					// ob[gdpData[i].Country] = +gdpData[i].Value
+
+					// if(gdpData[i].Year==selectedYear) {
+						// targetGDP.push({Country:gdpData[i].Country,value:+gdpData[i].Value})
+						// targetGDP[gdpData[i].Country] = +gdpData[i].Value
+					// }
+					// console.log(targetGDP)
 				}
 				return targetGDP
 			}
 
 			function getMaxGDP(targetGDP) {
+				// @ts-ignore
 				return Math.max(...Object.values(targetGDP))
 			}
 
-			var selectedYear = 2000;
-			var targetGDP = getTargetGDP(selectedYear)
+			var selectedYear = "2000";
+			var allGDP = getTargetGDP()
+			// var targetGDP = allGDP
+			var targetGDP = allGDP[selectedYear]
+			console.log(allGDP)
+
 			var maxGDP = getMaxGDP(targetGDP)
+			// 
 			
 			console.log(maxGDP)
 
@@ -84,6 +246,10 @@ export class Chart3Component implements OnInit {
 						.append("path")
 						.attr("class","country")
 						.attr("d",path)
+						.attr("value", function(d):any {
+							var a:any = d
+							return a.properties.sovereignt
+						})
 						.attr("fill",function(d):any{
 							var a:any = d
 							// console.log(a.properties.name)
@@ -110,7 +276,7 @@ export class Chart3Component implements OnInit {
 						.on("mouseover",function(){
 							var selected:any = this
 							
-							// console.log(selected.__data__.properties.name)
+							console.log(selected.__data__.properties.name)
 
 							// d3.select(selected)
 								// .attr("fill","red")
@@ -125,6 +291,7 @@ export class Chart3Component implements OnInit {
 							// var selected:any = this
 							// d3.select(selected)
 								// .attr("fill","green")
+
 						})
 
 			// var selection = d3.select('select').property('value');
@@ -135,7 +302,108 @@ export class Chart3Component implements OnInit {
 			d3.selectAll('.yearButton')
 				.on("click",function() {
 					// draw heat map with updated year
-					console.log(d3.select(this).property('value'))
+					
+					selectedYear = d3.select(this).property('value')
+					console.log(selectedYear)
+					targetGDP = allGDP[selectedYear]
+					maxGDP = getMaxGDP(targetGDP)
+					colorScale = d3.scaleLinear()
+									.domain([0,maxGDP])
+									// @ts-ignore
+									.range([0,1])
+					svg.selectAll(".country")
+						.remove()
+					svg.selectAll(".country")
+						.data(countries)
+						.enter()
+						.append("path")
+						.attr("class","country")
+						.attr("d",path)
+						.attr("value", function(d):any {
+							var a:any = d
+							return a.properties.sovereignt
+						})
+						.attr("fill",function(d):any{
+							var a:any = d
+							// console.log(a.properties.name)
+							// if(contryList.indexOf(a.properties.sovereignt)!=-1 && targetGDP[a.properties.sovereignt]) {
+							// 	console.log(targetGDP[a.properties.sovereignt])
+							// 	console.log(a.properties.sovereignt)
+							// 	return d3.interpolateReds(colorScale(targetGDP[a.properties.sovereignt]))
+							// }
+							// return 'None'
+
+							if(targetGDP[a.properties.sovereignt]) {
+								// console.log(targetGDP[a.properties.sovereignt])
+								// console.log(a.properties.sovereignt)
+								return d3.interpolateBlues(colorScale(targetGDP[a.properties.sovereignt]))
+							}
+							return 'None'
+
+							// console.log(targetGDP[a.properties.name])
+							// console.log(colorScale(targetGDP[a.properties.name]))
+							// console.log(d3.schemeReds[colorScale(targetGDP[a.properties.name])])
+							
+						})
+						.attr("stroke","black")
+						.on("mouseover",function(){
+							var selected:any = this
+							
+							console.log(selected.__data__.properties.name)
+
+							// d3.select(selected)
+								// .attr("fill","red")
+						})
+						.on("mouseout",function(){
+							var selected:any = this
+
+							// d3.select(selected)
+								// .attr("fill","None")
+						})
+						.on("click",function(){
+							// var selected:any = this
+							// d3.select(selected)
+								// .attr("fill","green")
+
+						})
+					// 	// .attr("d",path)
+					// 	.attr("fill",function(d):any{
+					// 		console.log(d)
+
+					// 		return "None"
+					// 	})
+
+						// .property('value')
+					// console.log(path)
+						// .data(countries)
+						// .enter()
+						// .append("path")
+						// .attr("class","country")
+						// .attr("d",path)
+						// .attr("fill",function(d):any{
+							// var a:any = d
+							// console.log(a.properties.name)
+							// if(contryList.indexOf(a.properties.sovereignt)!=-1 && targetGDP[a.properties.sovereignt]) {
+							// 	console.log(targetGDP[a.properties.sovereignt])
+							// 	console.log(a.properties.sovereignt)
+							// 	return d3.interpolateReds(colorScale(targetGDP[a.properties.sovereignt]))
+							// }
+							// return 'None'
+
+							// if(targetGDP[a.properties.sovereignt]) {
+								// console.log(targetGDP[a.properties.sovereignt])
+								// console.log(a.properties.sovereignt)
+								// return d3.interpolatePuOr(colorScale(targetGDP[a.properties.sovereignt]))
+							// }
+							// return 'None'
+
+							// console.log(targetGDP[a.properties.name])
+							// console.log(colorScale(targetGDP[a.properties.name]))
+							// console.log(d3.schemeReds[colorScale(targetGDP[a.properties.name])])
+							
+						// // })
+						// .attr("stroke","gray")
+						// .exit()
 					
 				})
 
@@ -144,7 +412,7 @@ export class Chart3Component implements OnInit {
 		
 
 
-	})
+	}) //////
 
   }
 
